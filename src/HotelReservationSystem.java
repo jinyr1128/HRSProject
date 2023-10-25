c09a0d5fef15905cfbb0feeef0f7552e076601f7
dmlal <opkl8781@naver.com>
2023-10-25 10:31:51 +0900

    틀리면 다시 바로바로 재입력되도록 변경

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -22,25 +22,14 @@ public class HotelReservationSystem {
                 case 1:// 예약하기
                     String name = inputInfo("이름을 입력하세요", scanner);
                     //아래 메소드에서 설명
-                    String phoneNumber = "";
-                    int money = 0;
-                    try {
-                        phoneNumber = inputPhoneInfo(scanner);
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                        break;
-                    }
-                    // 금액대신 문자를 입력하는 경우를 방지.
-                    try {
-                        money = inputMoneyInfo("소지 금액을 입력하세요: ", scanner);
-                    } catch (InputMismatchException e) {
-                        System.out.println("올바른 숫자를 입력해주세요");
-                        break;
-                    }
+                    String  phoneNumber = inputPhoneInfo(scanner);
+
+
+                    int money = inputMoneyInfo("소지 금액을 입력하세요: ", scanner);
                     scanner.nextLine();
 
 
-                    System.out.print("예약 날짜를 입력하세요 (예, 2023-10-27): ");
+                    System.out.print("예약 날짜를 입력하세요 (예, 2023-11-01): ");
                     String date = scanner.nextLine();
                     System.out.println("date = " + date);
                     System.out.println("해당 날짜에 예약 가능한 방은 다음과 같습니다.");
@@ -53,17 +42,16 @@ public class HotelReservationSystem {
 
                     if (i == 1) {                                                 // i가 증가되지 않았을 경우 예약 가능한 방이 존재하지 않음
                         System.out.println("\n해당 날짜에 예약 가능한 방이 존재하지 않습니다.");
-                        System.out.print("다른 날짜를 입력하세요 (예, 2023-10-27): ");
+                        System.out.print("다른 날짜를 입력하세요 (예, 2023-11-01): ");
                         date = scanner.nextLine();                              // 날짜를 다시 입력받음
                     }
 
-                    System.out.print("\n방 키를 입력하세요 : ");
-                    String roomKey = scanner.nextLine();
+
+                    String roomKey = inputRoomkeyInfo("\n방 키를 입력하세요 : ", scanner);
 
                     Customer customer = new Customer(name, phoneNumber, money);
                     // 예약 시도 및 결과 출력
                     date = date.strip();
-                    System.out.println("date = " + date);
                     UUID reservationId = hotel.reserveRoom(roomKey, customer, date);
                     if (reservationId != null) {
                         System.out.println("예약 성공! 예약 ID는 다음과 같습니다: " + reservationId);
@@ -137,41 +125,66 @@ public class HotelReservationSystem {
         return sc.nextLine();
     }
 
-
-
-    /*
-    튜터님께서 정규화를 생성자에서 받아오는 것보다 메인에서 메소드를 따로 파서 이것을 활용하는게 좋다고 하셔서 따로 팠습니다.
-    return null  break 등 여러 방법을 써봤는데  휴대폰번호 형식을 틀려도 계속 다음 스위치문이 이어져서
-    정규화된 000-0000-0000 형식과 sc에서 입력된 휴대폰번호 형식이 다를 경우
-    에러를 생성해서 catch문에서 break하는 형식으로 사용했습니다.
-     */
-    private static String inputPhoneInfo(Scanner sc) throws IllegalArgumentException {
-        System.out.println("휴대폰 번호를 입력하세요");
-
-        //정규화 추가    휴대폰 형식이 000-0000-0000이 아니면 입력이 불가능
-//        String regexPhoneNum = " ^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$";
-        String regexPhoneNum = "^010-[0-9]{4}-[0-9]{4}$";
-        String checkPhoneNum = sc.nextLine();
-        if (!Pattern.matches(regexPhoneNum, checkPhoneNum)) {
-            throw new IllegalArgumentException("휴대폰 번호 형식이 일치하지 않습니다.");
-        } else {
-            return checkPhoneNum;
+    private static String inputRoomkeyInfo(String Info, Scanner sc) {   // 아래 체크룸키에서 TF가 리턴되면 여기서 최종적으로 리턴
+        System.out.println(Info);
+        while (true) {
+            String roomKey = sc.nextLine();
+            if (checkRoomkey(roomKey)) {
+                return roomKey;
+            } else {
+                System.out.println("다시 입력해주세요. (ex: small1, small2)");
+            }
         }
     }
 
+    private static boolean checkRoomkey(String roomKey) {           // inputRoomkeyInfo 에서 입력된 값과 room key값 비교
+        Hotel hotel = new Hotel();
+        hotel.roomKeySet();
+        for (String checkRoomkey : hotel.roomKeySet()) {
+            if (checkRoomkey.equals(roomKey)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    private static String inputPhoneInfo(Scanner sc) {  //정규화 추가    휴대폰 형식이 000-0000-0000이 아니면 입력이 불가능
+        System.out.println("휴대폰 번호를 입력하세요");
+//      String regexPhoneNum = " ^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$";         둘 다 되는거 같네요 아마도?
+        String regexPhoneNum = "^010-[0-9]{4}-[0-9]{4}$";
+        while(true) {
+            String checkPhoneNum = sc.nextLine();
+            if (Pattern.matches(regexPhoneNum, checkPhoneNum)) {
+                return checkPhoneNum;
+            } else {
+                System.out.println("휴대폰 번호 형식이 일치하지 않습니다.\n다시 입력해주세요.");
+            }
+        }
+    }
+
+
     private static int inputMoneyInfo(String Info, Scanner sc) {
         System.out.println(Info);
-        return sc.nextInt();
+        while (true) {
+            if (sc.hasNextInt()) {
+                return sc.nextInt();
+            } else {
+                System.out.println("숫자를 입력해주세요");
+                sc.nextLine();
+            }
+        }
     }
+        private static void printMenu () {
+            System.out.println("호텔 예약 시스템에 오신 것을 환영합니다!");
+            System.out.println("1. 예약하기");
+            System.out.println("2. 예약 취소하기");
+            System.out.println("3. 나의 예약 보기");
+            System.out.println("4. 종료");
+            System.out.println("0. 모든 예약 보기 (관리자)");
+            System.out.print("선택을 입력하세요: ");
 
-    private static void printMenu() {
-        System.out.println("호텔 예약 시스템에 오신 것을 환영합니다!");
-        System.out.println("1. 예약하기");
-        System.out.println("2. 예약 취소하기");
-        System.out.println("3. 나의 예약 보기");
-        System.out.println("4. 종료");
-        System.out.println("0. 모든 예약 보기 (관리자)");
-        System.out.print("선택을 입력하세요: ");
+        }
 
-    }
 }
